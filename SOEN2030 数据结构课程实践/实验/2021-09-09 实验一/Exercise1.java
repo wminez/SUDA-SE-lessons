81173b3ba6d6ee575104d46a1f74ca085d2fe1cb
Snowflyt <gaoge011022@163.com>
2022-09-09 17:05:19 +0800

    修正部分BUG

--- SOEN2030 数据结构课程实践/实验/2021-09-09 实验一/Exercise1.java
+++ SOEN2030 数据结构课程实践/实验/2021-09-09 实验一/Exercise1.java
@@ -3,7 +3,7 @@ public class Exercise1 {
     public static void main(String[] args) {
         double result = 0;
         for (int i = 100; i > 0; i--) {
-            result += 1.0 / i * i;
+            result += 1.0 / (i * i);
         }
         result = Math.sqrt(6 * result);
         System.out.println(result);
